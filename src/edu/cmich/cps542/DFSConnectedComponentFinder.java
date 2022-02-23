84b78fa27b4ce8bb333b72a9b326b7b377c5c65a
kelle1ds <kelle1ds@cmich.edu>
2022-02-23 13:57:49 -0500

    Adding stuff to the ICA

--- src/edu/cmich/cps542/DFSConnectedComponentFinder.java
+++ src/edu/cmich/cps542/DFSConnectedComponentFinder.java
@@ -2,48 +2,62 @@ package edu.cmich.cps542;
 
 public class DFSConnectedComponentFinder {
 
-	/** 
-	 * Labels the connected components in a graph such that all 
+	public static void main(String[] args) {
+
+		int size = 9;
+		String edgeSpecifier = ("0-1,1-2,2-3,2-0,2-5,4-5,5-4,2-4");
+		boolean directed = true;
+		Graph g = new Graph(size,edgeSpecifier,directed);
+		Integer[] labels = findConnectedComponentsWDFS(g);
+	}
+
+	/**
+	 * Labels the connected components in a graph such that all
 	 *   members of a connected component of the graph share the same
 	 *   label.  Traverses graph in a depth-first manner.
-	 * 
+	 *
 	 * @param g the graph in which to identify connected components
 	 * @return labels for each vertex in the graph denoting connected component membership
 	 */
-
-	public static void main(String[] args) {
-		int size = 9;
-		String edgeSpecifier = ("0-1,1-2,2-3,3-0,2-4,4-5,5-6,6-4,7-6,7-8");
-
-		boolean directed = true;
-		Graph g = new Graph(size,edgeSpecifier,directed);
-		Integer[] order = null;
-		order = BFS.ProcessOrderInBFS(g);
+	public static Integer[] findConnectedComponentsWDFS(Graph g) {
 
 		Integer[][] adjMatrix = g.getAdjMatrix();
+		int size = adjMatrix.length;
 
-		int transpose[][]=new int[size][size];
-		//Code to transpose a matrix
+		int transpose[][]=new int[size][size];  //transposed matrix
+
+		//Code to transpose the adjMatrix
 		for(int i=0;i<size;i++) {
 			for (int j = 0; j < size; j++) {
 				transpose[i][j] = adjMatrix[j][i];
 			}
 		}
 
-		for(int i = 0; i < size;i++){
-			for(int j = 0; j < size; j++)
-				System.out.println(transpose[i][j] + " " + adjMatrix[i][j]);
-		}
-
-	}
-
-	public static Integer[] findConnectedComponentsWDFS(Graph g) {
+		printMatrix(adjMatrix);
 
 		int componentID = 0;
-		Integer[] componentMembership = new Integer[g.size()]; 
+		Integer[] componentMembership = new Integer[g.size()];
 
+		Integer[] order = null;
+		order = BFS.ProcessOrderInBFS(g);
+
+		int k = BFS.bfs(g,0, 0,order);
+		//System.out.println("k = " + k);
+
+		for(int i = 0; i< order.length;i++){
+			System.out.println(order[i]);
+		}
 		
 		return componentMembership;
+	}
+
+	public static void printMatrix(Integer matrix[][]){
+		for (int i = 0; i < matrix.length; i++) { //this equals to the row in our matrix.
+			for (int j = 0; j < matrix[i].length; j++) { //this equals to the column in each row.
+				System.out.print(matrix[i][j] + " ");
+			}
+			System.out.println(); //change line on console as row comes to end in the matrix.
+		}
 	}
 
 	
